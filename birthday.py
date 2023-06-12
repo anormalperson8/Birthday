0eb73f1326563a588afa7195c6efcd76bc4a6db4
anormalperson8 <133204516+anormalperson8@users.noreply.github.com>
2023-06-12 20:15:20 +0800

    Added birthday retrieval and birthday validation

--- birthday.py
+++ birthday.py
@@ -1,1 +1,28 @@
+import datetime
+import json
+
+id = "id"
+month = "month"
+day = "day"
+year = "year"
+
+
+def turn_to_english(number):
+    x = datetime.datetime(1, number, 1).strftime("%B")
+    return x
+
+
+def get_date(user_id):
+    f = open("/home/sunny/PythonBday/data/bday.json", 'r')
+    data = json.load(f)
+    for people in data["people"]:
+        if people[id] == user_id:
+            return datetime.datetime(people[year], people[month], people[day])
+    return None
+
+
+# debug print
+# print(get_date(1117746108161081344).strftime("%Y"))
+# print(datetime.datetime.now())
+
 
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import nextcord
 import os
 from dotenv import load_dotenv
 from nextcord.ext import commands
-import json
 import datetime
+import birthday
+import calendar
 
 intents = nextcord.Intents.all()
 client = commands.Bot(command_prefix='.', intents=intents, activity=nextcord.Game(name="Happy Birthday...?"))
@@ -39,10 +40,75 @@ async def ping(interaction: nextcord.Interaction):
 
 
 @commands.guild_only()
-@client.slash_command(guild_ids=guilds_list, description="Get a member's birthday")
-async def get_birthday(interaction: nextcord.Interaction, arg: nextcord.User):
+@client.slash_command(guild_ids=guilds_list, description="Get a member's birthday! You can even get mine!")
+async def get_birthday(interaction: nextcord.Interaction, user: nextcord.User):
     await interaction.response.defer()
-    await interaction.edit_original_message(content=f"Pong!")
+    if interaction.user.id != owner_id and interaction.user.id != int("805783359610552380"):
+        await interaction.edit_original_message(content="You don't have permission yet! Stay tuned.")
+        return
+    # TODO: ADD BLOCKING OF MEMBERS NOT IN THE SERVER
+    date = birthday.get_date(user.id)
+    if date is not None:
+        day = int(date.day)
+        month = date.strftime("%B")
+        year = int(date.strftime("%Y"))
+    else:
+        await interaction.edit_original_message(
+            content=f"{user.display_name}'s birthday does not exist in the system. <:EeveeCry:965985819057848320>")
+        return
+    postfix = 'th'
+    if (day % 10 == 1) and (day // 10 != 1):
+        postfix = 'st'
+    elif (day % 10 == 2) and (day // 10 != 1):
+        postfix = 'nd'
+    elif (day % 10 == 3) and (day // 10 != 1):
+        postfix = 'rd'
+    year_phrase = ""
+    if year != 1:
+        year_phrase = ", " + str(year)
+    await interaction.edit_original_message(
+        content=f"{user.display_name}'s birthday is on {day}{postfix} {month}{year_phrase}.")
+
+
+def valid_date(year, month, day):
+    if year < 1 or month < 1 or month > 12 or year > 2023 or day < 1 or day > 31:
+        return False
+    if month == 2 and day > 28:
+        if not calendar.isleap(year):
+            return False
+        return True
+    if month % 2 == 0 and month <= 7 and day > 30:
+        return False
+    if month % 2 == 1 and month > 7 and day > 30:
+        return False
+    if datetime.datetime.now() < datetime.datetime(year=year, month=month, day=day):
+        return False
+    return True
+
+
+@commands.guild_only()
+@client.slash_command(guild_ids=guilds_list, description="Set your own birthday!")
+async def set_birthday(interaction: nextcord.Interaction, user: nextcord.User, day: int, month: int, year: int = 1):
+    # TODO: SETUP THE SET BIRTHDAY
+    # TODO: CHECK INTERACTION CHANNELS
+    await interaction.response.defer()
+    if not valid_date(year, month, day):
+        await interaction.edit_original_message(content="Invalid Birthday! <:EeveeOwO:965977455791857695>")
+        return
+    await interaction.edit_original_message(content=f"This hasn't been implemented yet. Stay tuned!")
+
+
+@commands.guild_only()
+@client.slash_command(guild_ids=guilds_list, description="Set your own birthday")
+async def set_user_birthday(interaction: nextcord.Interaction, user: nextcord.User, day: int, month: int,
+                            year: int = 1):
+    # TODO: SETUP THE SET BIRTHDAY (MOD'S VERSION)
+    # TODO: CHECK INTERACTION CHANNEL
+    await interaction.response.defer()
+    if not valid_date(year, month, day):
+        await interaction.edit_original_message(content="Invalid Birthday! <:EeveeOwO:965977455791857695>")
+        return
+    await interaction.edit_original_message(content=f"This hasn't been implemented yet. Stay tuned!")
 
 
 @client.listen('on_message')
@@ -54,5 +120,6 @@ async def on_message(message):
         name = message.author.name
     if message.content == "Hi Bday":
         await message.channel.send(f"Hello {name}!")
+
 
 client.run(token)
