d48d8442fff89207806fde587bc8917f1a9cc84e
anormalperson8 <133204516+anormalperson8@users.noreply.github.com>
2023-10-03 01:00:07 +0800

    Renamed the file; Added searching functions

--- server_info.py
+++ server_info.py
@@ -4,13 +4,12 @@ from dotenv import load_dotenv
 
 path = os.path.dirname(os.path.abspath(__file__))
 data_path = f"{path}/data/"
-load_dotenv(f"{data_path}data.env")
 
 
 class Server:
     def __init__(self, server_id: int, announcement_channel: int, moderator_roles: list,
                  allowed_channels: list, role_to_ping: int):
-        self.server_id = server_id
+        self.serverID = server_id
         self.announcementChannel = announcement_channel
         self.moderatorRoles = moderator_roles
         self.allowedChannels = allowed_channels
@@ -27,3 +26,17 @@ def get_servers():
                               server["moderatorRoles"], server["allowedChannels"],
                               server["RoleToPing"]))
     return servers
+
+
+# Returns the index of the server in the list of servers
+def search_for_server_id(servers: list, server_id: int):
+    return [i for i in range(len(servers)) if servers[i].serverID == server_id][0]
+
+
+# Returns the server object in the list of servers
+def search_for_server(servers: list, server_id: int):
+    return [i for i in servers if i.serverID == server_id][0]
+
+
+
+
